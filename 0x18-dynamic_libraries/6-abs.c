d1f99ec43a2e2a3039feaa978bc60390b99953e3
oladakinjoye <oladakinjoye@gmail.com>
2023-11-14 18:53:19 -0800

    task 0

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @k: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int k)
+{
+	if (k >= 0)
+	{
+		return (k);
+	}
+	return (-k);
+}
